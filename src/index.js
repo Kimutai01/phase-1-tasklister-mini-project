e96467c03a0c8d55437a5b87d85f82a5a3be0288
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-22 09:05:32 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const btn = document.querySelector(".btn");
+  const desc = document.querySelector("#new-task-description");
+  const tasksDiv = document.querySelector("#tasks");
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const list = document.createElement("li");
+    list.textContent = desc.value;
+    tasksDiv.appendChild(list);
+  });
 });
